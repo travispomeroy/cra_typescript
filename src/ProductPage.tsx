80678e5910285f57e9cad45ce9b9b322692fc4ff
travispomeroy <travis.pomeroy84@gmail.com>
2020-02-29 18:26:34 -0500

    Added a product page to display details about a product.

---
+++ src/ProductPage.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from "react";
+import {RouteComponentProps} from "react-router-dom";
+import {mockData, Product} from "./ProductsData";
+
+type Props = RouteComponentProps<{id: string}>;
+
+const ProductPage: React.FC<Props> = ({match: {params}}: Props) => {
+    const [product, setProduct] = useState<Product | undefined>(undefined);
+    const [isAdded, setAdded] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (params.id) {
+            const id: number = parseInt(params.id, 10);
+            const productToDisplay: Product = mockData.filter(value => value.id === id)[0];
+            setProduct(productToDisplay);
+        }
+    }, [params.id]);
+
+    const handleAddClick = () => {
+        setAdded(true);
+    };
+
+    return (
+        <div className="page-container">
+            {product ? (
+                <>
+                    <h1>{product?.name}</h1>
+                    <p>{product?.description}</p>
+                    <p className="product-price">
+                        {new Intl.NumberFormat("en-US", {
+                            currency: "USD",
+                            style: "currency"
+                        }).format(product?.price)}
+                    </p>
+                    {!isAdded && (
+                        <button type="button" onClick={handleAddClick}>
+                            Add to basket
+                        </button>
+                    )}
+                </>
+            ) : (
+                <p>Product not found!</p>
+            )}
+        </div>
+    );
+};
+
+export default ProductPage;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,13 +3,15 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
+import ProductPage from "./ProductPage";
 
 const Routes: React.FC = () => {
     return (
         <Router>
             <div>
                 <Header />
-                <Route path="/products" component={ProductsPage} />
+                <Route exact path="/products" component={ProductsPage} />
+                <Route path="/products/:id" component={ProductPage} />
                 <Route path="/admin" component={AdminPage} />
             </div>
         </Router>
