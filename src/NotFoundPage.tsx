f3e8815c089afc8f9ed78a8b6a166bf80d9910a1
travispomeroy <travis.pomeroy84@gmail.com>
2020-02-29 18:33:16 -0500

    Added a not found page and a redirect to homepage from root context.

---
+++ src/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+
+    return (
+        <div className="page-container">
+            <h1>Sorry, this page cannot be found</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
 import ProductPage from "./ProductPage";
+import NotFoundPage from "./NotFoundPage";
 
 const Routes: React.FC = () => {
     return (
         <Router>
             <div>
                 <Header />
-                <Route exact path="/products" component={ProductsPage} />
-                <Route path="/products/:id" component={ProductPage} />
-                <Route path="/admin" component={AdminPage} />
+                <Switch>
+                    <Redirect to="/products" from="/" exact />
+                    <Route exact path="/products" component={ProductsPage} />
+                    <Route path="/products/:id" component={ProductPage} />
+                    <Route path="/admin" component={AdminPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
             </div>
         </Router>
     );
