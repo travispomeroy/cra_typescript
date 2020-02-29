de1c48b70fd66c604989a6216cf0bae372d2b0fb
travispomeroy <travis.pomeroy84@gmail.com>
2020-02-29 18:39:02 -0500

    Added a login page and conditional routing if the user is not logged in.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
 import ProductPage from "./ProductPage";
 import NotFoundPage from "./NotFoundPage";
+import LoginPage from "./LoginPage";
 
 const Routes: React.FC = () => {
+    const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+
     return (
         <Router>
             <div>
+                <button type="button" onClick={() => setLoggedIn(!isLoggedIn)}>Login</button>
                 <Header />
                 <Switch>
                     <Redirect to="/products" from="/" exact />
                     <Route exact path="/products" component={ProductsPage} />
                     <Route path="/products/:id" component={ProductPage} />
-                    <Route path="/admin" component={AdminPage} />
+                    <Route path="/admin" component={AdminPage}>
+                        {isLoggedIn ? <AdminPage /> : <Redirect to="/login" />}
+                    </Route>
+                    <Route path="/login" component={LoginPage} />
                     <Route component={NotFoundPage} />
                 </Switch>
             </div>
