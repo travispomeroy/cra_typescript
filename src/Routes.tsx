e4e6c367bc5dcb3604074f10974872cdcb9674dc
travispomeroy <travis.pomeroy84@gmail.com>
2020-02-29 18:04:40 -0500

    Added a header section to the site and some routes.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
+import Header from "./Header";
 
 const Routes: React.FC = () => {
     return (
         <Router>
             <div>
+                <Header />
                 <Route path="/products" component={ProductsPage} />
                 <Route path="/admin" component={AdminPage} />
             </div>
