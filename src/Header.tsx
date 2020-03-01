449add59bc3f15d190a65bef25c1bb0d230388d6
travispomeroy <travis.pomeroy84@gmail.com>
2020-02-29 19:18:16 -0500

    Added a search bar to main page to filter out products to search by.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import {NavLink} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {NavLink, RouteComponentProps, withRouter} from "react-router-dom";
 import logo from "./logo.svg";
 
-const Header: React.FC = () => {
+const Header: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
+    const [search, setSearch] = useState<string>("");
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(props.location.search);
+        setSearch(searchParams.get("search") || "");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.currentTarget.value);
+    };
+
+    const handleSearchKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            props.history.push(`/products?search=${search}`);
+        }
+    };
+
     return (
         <header className="header">
+            <div className="search-container">
+                <input
+                    type="search"
+                    placeholder="search"
+                    value={search}
+                    onChange={handleSearchChange}
+                    onKeyDown={handleSearchKeydown}
+                />
+            </div>
             <img src={logo} className="header-logo" alt="logo" />
             <h1 className="header-title">React Shop</h1>
             <nav>
-                <NavLink to="/products" className="header-link" activeClassName="header-link-active">
+                <NavLink
+                    to="/products"
+                    className="header-link"
+                    activeClassName="header-link-active"
+                >
                     Products
                 </NavLink>
                 <NavLink to="/admin" className="header-link" activeClassName="header-link-active">
@@ -19,4 +50,4 @@ const Header: React.FC = () => {
     );
 };
 
-export default Header;
+export default withRouter(Header);
--- src/ProductsPage.tsx
+++ src/ProductsPage.tsx
@@ -1,23 +1,43 @@
 import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, RouteComponentProps} from "react-router-dom";
 import {Product, mockData} from "./ProductsData";
 
-const ProductsPage: React.FC = () => {
+const ProductsPage: React.FC<RouteComponentProps> = ({location: {search}}: RouteComponentProps) => {
     const [products, setProducts] = useState<Product[]>([]);
+    const [searchParam, setSearchParam] = useState<string>("");
 
     useEffect(() => {
         setProducts(mockData);
     }, []);
 
+    useEffect(() => {
+        const searchParams = new URLSearchParams(search);
+        setSearchParam(searchParams.get("search") || "");
+    }, [search]);
+
     return (
         <div className="page-container">
             <p>Welcome to React Shop where you can get all your tools for ReactJS!</p>
             <ul className="product-list">
-                {products.map(value => (
-                    <Link key={value.id} className="product-list-item" to={`/products/${value.id}`}>
-                        {value.name}
-                    </Link>
-                ))}
+                {products.map(value => {
+                    if (
+                        !searchParam ||
+                        (searchParam &&
+                            value.name.toLowerCase().indexOf(searchParam.toLowerCase()) > -1)
+                    ) {
+                        return (
+                            <Link
+                                key={value.id}
+                                className="product-list-item"
+                                to={`/products/${value.id}`}
+                            >
+                                {value.name}
+                            </Link>
+                        );
+                    }
+
+                    return null;
+                })}
             </ul>
         </div>
     );
