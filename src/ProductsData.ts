d840122d359cf59461a5b2ed046776a6c76a3c95
travispomeroy <travis.pomeroy84@gmail.com>
2020-03-01 11:46:19 -0500

    Added reviews to the data model.

--- src/ProductsData.ts
+++ src/ProductsData.ts
@@ -3,25 +3,62 @@ export interface Product {
     name: string;
     description: string;
     price: number;
+    reviews: Review[];
+}
+
+export interface Review {
+    comment: string;
+    reviewer: string;
 }
 
 export const mockData: Product[] = [
     {
-        description: "A collection of navigational components that compose declaratiely with your app",
+        description: "A collection of navigational components that compose declaratively with your app",
         id: 1,
         name: "React Router",
-        price: 8
+        price: 8,
+        reviews: [
+            {
+                comment: "Excellent!  This does everything I want.",
+                reviewer: "Billy"
+            },
+            {
+                comment: "The best router I've ever worked with.",
+                reviewer: "Sally"
+            }
+        ]
     },
     {
         description: "A library that helps manage state across your app",
         id: 2,
         name: "React Redux",
-        price: 12
+        price: 12,
+        reviews: [
+            {
+                comment: "I've founc this really useful in a large app I'm working on.",
+                reviewer: "Billy"
+            },
+            {
+                comment: "A bit confusing at first but simple when you get used to it.",
+                reviewer: "Sally"
+            }
+        ]
     },
     {
         description: "A library that helps you interact with a GraphQL backend",
         id: 3,
         name: "React Apollo",
-        price: 12
+        price: 12,
+        reviews: [
+            {
+                comment: "I'll never work with a REST API again!",
+                reviewer: "Billy"
+
+            },
+            {
+                comment: "It makes working with GraphQL backends a breeze.",
+                reviewer: "Sally"
+            }
+        ]
     }
 ];
